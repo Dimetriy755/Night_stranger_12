98fd2e110c6f4ebb6b8a62ddfcc5d72cc8553afe
Dimetriy755 <dimetryi1212@gmail.com>
2021-02-19 00:36:33 +0300

    new files-5

---
+++ untitled.bat
@@ -0,0 +1,9 @@
+@ECHO OFF
+
+START C:\Users\User\Downloads\ansi153\x64\ansicon.exe -p
+
+TIMEOUT /T -1
+
+CMD /c java -jar C:\Users\User\IdeaProjects\untitled\out\artifacts\untitled_jar\untitled.jar
+
+
